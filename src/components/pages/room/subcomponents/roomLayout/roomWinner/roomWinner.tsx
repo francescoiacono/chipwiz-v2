5f1930b649800cf5e85de182f72752b4739cd7b2
Francesco <sennozeta@gmail.com>
2023-07-20 16:53:48 +0100

    Minor css fix for mobile

--- src/components/pages/room/subcomponents/roomLayout/roomWinner/roomWinner.tsx
+++ src/components/pages/room/subcomponents/roomLayout/roomWinner/roomWinner.tsx
@@ -1,14 +1,12 @@
-import Button from '@/components/ui/button/button';
+import WinnerTitle from './winnerTitle/winnerTitle';
+import WinnerSelector from './winnerSelector/winnerSelector';
 import { useRoom } from '@/components/providers';
 import { Player, Stage } from '@/data/types';
 import { useHand } from '@/components/hooks/gameActions';
-import { useCallback, useEffect } from 'react';
-import WinnerTitle from './winnerTitle/winnerTitle';
-import WinnerSelector from './winnerSelector/winnerSelector';
 
 const RoomWinner = () => {
   const { room } = useRoom();
-  const { startHand, setHandWinner } = useHand();
+  const { setHandWinner } = useHand();
 
   // Handle winner selection
   const handleWinnerSelection = async (player: Player) => {
