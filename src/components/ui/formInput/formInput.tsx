6d00b2474018c527a86c558d571a231d2994ad29
Francesco <sennozeta@gmail.com>
2023-07-21 14:51:14 +0100

    Numeric Inputs will now delete the initial 0 on mobile

--- src/components/ui/formInput/formInput.tsx
+++ src/components/ui/formInput/formInput.tsx
@@ -18,11 +18,16 @@ const FormInput = ({
   placeholder,
   children,
 }: FormInputProps) => {
+  const handleChangeCorrectly = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleChange(e);
+    if (type === 'number') e.target.value = e.target.value.substring(1);
+  };
+
   return (
     <div className={styles.formInput}>
       {children && <label>{children}</label>}
       <Input
-        onChange={handleChange}
+        onChange={handleChangeCorrectly}
         type={type || 'text'}
         name={name}
         placeholder={placeholder || '...'}
